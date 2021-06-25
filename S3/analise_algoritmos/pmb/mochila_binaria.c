6c4136c2cc24f1b6e280c8d3bbd0938386efe905
João Breno <joao.breno.rodrigues07@aluno.ifce.edu.br>
2021-06-25 14:33:07 -0300

    pmb

---
+++ S3/analise_algoritmos/pmb/mochila_binaria.c
@@ -0,0 +1,82 @@
+#include <stdio.h>		// printf, scanf
+#include <string.h>		// memset
+#include <stdlib.h>		// malloc
+#define MAX 101
+#define INF 1e+6		// entenda infinito como inatingível
+
+typedef struct b_tree
+{
+	int d;
+	
+	struct b_tree *pega;
+	struct b_tree *pass;
+} b_tree;
+
+struct
+{
+	int p, v;
+} itens[MAX];
+
+int n, c_max;
+int pd[MAX][MAX];
+
+int mochila(int i, int c, b_tree *no)
+{
+	if (c < 0)
+		return -INF;
+	
+	if (i >= n)
+		return 0;
+	
+	if (pd[i][c] != -1)
+		return pd[i][c];
+	
+	no->pega = (b_tree *) malloc(sizeof(b_tree));
+	no->pass = (b_tree *) malloc(sizeof(b_tree));
+	
+	int pega = itens[i].v + mochila(i+1, c - itens[i].p, no->pega);
+	int pass = mochila(i+1, c, no->pass);
+	
+	if (pega > pass)
+	{
+		no->d = 1;
+		return pd[i][c] = pega;
+	}
+	
+	no->d = 0;
+	return pd[i][c] = pass;
+}
+
+void print_mochila(int i, b_tree *no)
+{
+	if (no == NULL || i >= n)
+		return;
+	
+	if (no->d)
+	{
+		printf("item: %i, valor: %i, peso: %i\n", i, itens[i].v, itens[i].p);
+		print_mochila(i+1, no->pega);
+		return;
+	}
+	
+	print_mochila(i+1, no->pass);
+}
+
+int main()
+{
+	memset(pd, -1, MAX*MAX*sizeof(int));
+	
+	scanf("%i %i", &n, &c_max);
+	
+	for (int i = 0; i < n; i++)
+		scanf("%i %i", &itens[i].p, &itens[i].v);
+	
+	b_tree *bt = (b_tree *) malloc(sizeof(b_tree));
+	int ans = mochila(0, c_max, bt);
+	
+	print_mochila(0, bt);
+	printf("total: %i\n", ans);
+	
+	return 0;
+}
+
